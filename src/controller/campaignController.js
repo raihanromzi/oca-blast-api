c1ca5fde3481c5095168ec206912bb064555cc76
Raihan Romzi R <raihan.rakhman16@gmail.com>
2024-08-29 17:23:10 +0700

    finish init

--- src/controller/campaignController.js
+++ src/controller/campaignController.js
@@ -1,7 +1,17 @@
 import { logger } from '../application/logging.js'
+import { prismaClientMongo } from '../application/prisma.js'
 
 const createCampaign = async (req, res, next) => {
     try {
+        const test = await prismaClientMongo.report.create({
+            data: {
+                campaignId: 1,
+                reportData: {
+                    name: 'test',
+                },
+                createdAt: new Date(),
+            },
+        })
         res.status(200).json({ message: 'Campaign created successfully!' })
         logger.info(`Campaign created successfully!`)
     } catch (e) {
